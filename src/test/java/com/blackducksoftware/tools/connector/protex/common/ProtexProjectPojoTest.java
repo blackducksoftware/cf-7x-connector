496bf09f853303619aff2b5c90f6dd50ef2315b1
stevebillings <sbillings@blackducksoftware.com>
2015-11-21 15:48:59 -0500

    SESU-134: added ProtexComponentPojoTest

---
+++ src/test/java/com/blackducksoftware/tools/connector/protex/common/ProtexProjectPojoTest.java
@@ -0,0 +1,62 @@
+package com.blackducksoftware.tools.connector.protex.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.blackducksoftware.tools.connector.codecenter.common.ApprovalStatus;
+import com.blackducksoftware.tools.connector.protex.license.ProtexLicensePojo;
+import com.blackducksoftware.tools.connector.protex.license.ProtexLicensePojo.ApprovalState;
+
+public class ProtexProjectPojoTest {
+
+    private static final String TEST_LICENSE_NAME = "test license name";
+    private static final String VERSION_ID = "versionId";
+    private static final String NAME_ID = "nameId";
+    private static final String TEST_PRIMARY_LICENSE_ID = "testPrimaryLicenseId";
+    private static final String TEST_COMPONENT_DESCRIPTION = "test component description";
+    private static final String TEST_HOMEPAGE = "testHomepage";
+    private static final String TEST_COMPONENT_VERSION = "testComponentVersion";
+    private static final String TEST_COMPONENT_NAME = "testComponentName";
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Test
+    public void test() {
+	ComponentNameVersionIds nameVersionIds = new ComponentNameVersionIds(
+		NAME_ID, VERSION_ID);
+	List<ProtexLicensePojo> licenses = new ArrayList<>();
+	ProtexLicensePojo license = new ProtexLicensePojo("testLicenseId",
+		TEST_LICENSE_NAME, "test comment", "", "",
+		ApprovalState.APPROVED, "test license text");
+	licenses.add(license);
+	ProtexComponentPojo comp = new ProtexComponentPojo(
+		TEST_COMPONENT_NAME, TEST_COMPONENT_VERSION,
+		ApprovalStatus.APPROVED, TEST_HOMEPAGE, true, nameVersionIds,
+		licenses, ProtexComponentType.STANDARD,
+		TEST_COMPONENT_DESCRIPTION, TEST_PRIMARY_LICENSE_ID);
+
+	assertEquals(TEST_COMPONENT_NAME, comp.getName());
+	assertEquals(TEST_COMPONENT_VERSION, comp.getVersion());
+	assertEquals(ApprovalStatus.APPROVED, comp.getApprovalStatus());
+	assertEquals(TEST_HOMEPAGE, comp.getHomepage());
+	assertEquals(true, comp.isDeprecated());
+	assertEquals(NAME_ID, comp.getNameVersionIds().getNameId());
+	assertEquals(VERSION_ID, comp.getNameVersionIds().getVersionId());
+	assertEquals(TEST_LICENSE_NAME, comp.getLicenses().get(0).getName());
+	assertEquals(ProtexComponentType.STANDARD, comp.getType());
+	assertEquals(TEST_COMPONENT_DESCRIPTION, comp.getDescription());
+	assertEquals(TEST_PRIMARY_LICENSE_ID, comp.getPrimaryLicenseId());
+    }
+}
