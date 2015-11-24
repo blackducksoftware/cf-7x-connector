079d854eece746fe7a407a80f5798ef8074cd7a6
jrichard <jrichard@blackducksoftware.com>
2015-11-24 16:39:22 -0500

    Adding in unit test to ensure the attributeValuesByName field in ApplicationPojo does not change

--- src/test/java/com/blackducksoftware/tools/connector/codecenter/application/ApplicationPojoTest.java
+++ src/test/java/com/blackducksoftware/tools/connector/codecenter/application/ApplicationPojoTest.java
@@ -2,10 +2,12 @@ package com.blackducksoftware.tools.connector.codecenter.application;
 
 import static org.junit.Assert.assertEquals;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,10 +17,15 @@ import com.blackducksoftware.tools.connector.common.ApprovalStatus;
 public class ApplicationPojoTest {
 
     private static final String APP_VERSION = "TestAppVersion";
+
     private static final String APP_NAME = "Test Application";
+
     private static final String APP_ID = "testAppId";
+
     private static final String ATTR_VALUE = "testAttrValue";
+
     private static final String ATTR_NAME = "testAttrName";
+
     private static final String ATTR_ID = "testAttrId";
 
     @BeforeClass
@@ -31,17 +38,31 @@ public class ApplicationPojoTest {
 
     @Test
     public void test() {
-	List<AttributeValuePojo> attrValues = new ArrayList<>();
-	attrValues.add(new AttributeValuePojo(ATTR_ID, ATTR_NAME, ATTR_VALUE));
+        List<AttributeValuePojo> attrValues = new ArrayList<>();
+        attrValues.add(new AttributeValuePojo(ATTR_ID, ATTR_NAME, ATTR_VALUE));
 
-	ApplicationPojo app = new ApplicationPojo(APP_ID, APP_NAME,
-		APP_VERSION, attrValues, ApprovalStatus.PENDING);
+        ApplicationPojo app = new ApplicationPojo(APP_ID, APP_NAME,
+                APP_VERSION, attrValues, ApprovalStatus.PENDING);
 
-	assertEquals(APP_ID, app.getId());
-	assertEquals(APP_NAME, app.getName());
-	assertEquals(APP_VERSION, app.getVersion());
-	assertEquals(ApprovalStatus.PENDING, app.getApprovalStatus());
-	assertEquals(ATTR_VALUE, app.getAttributeByName(ATTR_NAME));
+        assertEquals(APP_ID, app.getId());
+        assertEquals(APP_NAME, app.getName());
+        assertEquals(APP_VERSION, app.getVersion());
+        assertEquals(ApprovalStatus.PENDING, app.getApprovalStatus());
+        assertEquals(ATTR_VALUE, app.getAttributeByName(ATTR_NAME));
     }
 
+    @Test
+    public void fieldNameCheck() {
+        try {
+            // The field name in ApplicationPojo should be attributeValuesByName
+            // Other utilities rely on this field name staying the same
+            Field field = ApplicationPojo.class.getDeclaredField("attributeValuesByName");
+            Assert.assertNotNull(field);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    // TODO fieldName check
 }
