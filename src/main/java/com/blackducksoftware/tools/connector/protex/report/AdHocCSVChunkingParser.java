81c11cf97efb98363de37598443c6644991d62b1
garoushanian <garoushanian@blackducksoftware.com>
2015-12-17 11:10:00 -0500

    #28: Minor update in getLimitedRowsFromReport() method;

--- src/main/java/com/blackducksoftware/tools/connector/protex/report/AdHocCSVChunkingParser.java
+++ src/main/java/com/blackducksoftware/tools/connector/protex/report/AdHocCSVChunkingParser.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Copyright (C) 2015 Black Duck Software, Inc.
  * http://www.blackducksoftware.com/
- *
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2 only
  * as published by the Free Software Foundation.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License version 2
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *******************************************************************************/
 package com.blackducksoftware.tools.connector.protex.report;
 
@@ -28,18 +28,18 @@ import com.google.common.base.Preconditions;
 
 /**
  * Extension of the main parser for chunking purposes only.
- *
+ * 
  * @author akamen
- *
+ * 
  * @param <T>
  */
 public class AdHocCSVChunkingParser<T extends HocElement> extends
-	AdHocCSVParser<T> {
+        AdHocCSVParser<T> {
     private final Logger log = LoggerFactory.getLogger(this.getClass()
-	    .getName());
+            .getName());
 
     /**
-     *
+     * 
      * @param hocElementClass
      * @param sectionName
      * @param report
@@ -47,44 +47,44 @@ public class AdHocCSVChunkingParser<T extends HocElement> extends
      * @throws Exception
      */
     public AdHocCSVChunkingParser(Class<T> hocElementClass, String sectionName,
-	    ReportPojo report, long rowChunk) throws Exception {
+            ReportPojo report, long rowChunk) throws Exception {
 
-	super(hocElementClass, sectionName);
-	super.rowChunk = rowChunk;
-	this.report = report;
+        super(hocElementClass, sectionName);
+        super.rowChunk = rowChunk;
+        this.report = report;
 
-	Preconditions.checkNotNull(report);
+        Preconditions.checkNotNull(report);
 
-	// Instantiate the reader
-	try {
-	    headerForChunking = parseHeader(AdHocCSVParser.CHUNKING);
+        // Instantiate the reader
+        try {
+            headerForChunking = parseHeader(AdHocCSVParser.CHUNKING);
 
-	} catch (Exception e) {
-	    log.error("Unable to created a limited parser: " + e.getMessage());
-	    throw new Exception(e);
-	}
+        } catch (Exception e) {
+            log.error("Unable to created a limited parser: " + e.getMessage());
+            throw new Exception(e);
+        }
 
     }
 
     /**
      * Limited rows, up until it hits rowChunk
-     *
+     * 
      * @param report
      * @return
      * @throws Exception
      */
     public List<T> getLimitedRowsFromReport(ReportPojo report) throws Exception {
-	this.report = report;
+        this.report = report;
 
-	List<T> rows = new ArrayList<T>();
-	try {
-	    rows = parseRows(headerForChunking, true);
-	} catch (Exception e) {
-	    log.error("General error in parsing CSV.");
-	    throw new Exception(e);
-	}
+        List<T> rows = new ArrayList<T>();
+        try {
+            rows = parseRows(headerForChunking, AdHocCSVParser.CHUNKING);
+        } catch (Exception e) {
+            log.error("General error in parsing CSV.");
+            throw new Exception(e);
+        }
 
-	return rows;
+        return rows;
     }
 
 }
