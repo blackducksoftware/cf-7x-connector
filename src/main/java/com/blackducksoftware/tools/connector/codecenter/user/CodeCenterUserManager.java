9abb1f66394aae752e3187ff3828d2270c18b20e
stevebillings <sbillings@blackducksoftware.com>
2016-07-06 16:08:07 -0400

    INTGEN-431: UserManager: Better logging

--- src/main/java/com/blackducksoftware/tools/connector/codecenter/user/CodeCenterUserManager.java
+++ src/main/java/com/blackducksoftware/tools/connector/codecenter/user/CodeCenterUserManager.java
@@ -44,161 +44,165 @@ import com.blackducksoftware.tools.connector.codecenter.CodeCenterAPIWrapper;
 import com.blackducksoftware.tools.connector.codecenter.common.ApplicationRolePojo;
 
 public class CodeCenterUserManager implements ICodeCenterUserManager {
-    private final Logger log = LoggerFactory.getLogger(this.getClass()
-            .getName());
+	private final Logger log = LoggerFactory.getLogger(this.getClass()
+			.getName());
 
-    private final CodeCenterAPIWrapper ccApiWrapper;
+	private final CodeCenterAPIWrapper ccApiWrapper;
 
-    private final Map<String, User> usersByIdCache = new HashMap<>();
+	private final Map<String, User> usersByIdCache = new HashMap<>();
 
-    private final Map<String, User> usersByNameCache = new HashMap<>();
+	private final Map<String, User> usersByNameCache = new HashMap<>();
 
-    public CodeCenterUserManager(CodeCenterAPIWrapper ccApiWrapper) {
-        this.ccApiWrapper = ccApiWrapper;
-    }
+	public CodeCenterUserManager(final CodeCenterAPIWrapper ccApiWrapper) {
+		this.ccApiWrapper = ccApiWrapper;
+	}
 
-    @Override
-    public String createUser(String username, String password, String firstName, String lastName, String email, boolean active) throws CommonFrameworkException {
-        UserCreate userCreate = new UserCreate();
-        userCreate.setName(username);
-        userCreate.setPassword(password);
-        userCreate.setEmail(email);
-        userCreate.setFirstName(firstName);
-        userCreate.setLastName(lastName);
-        userCreate.setActive(active);
+	@Override
+	public String createUser(final String username, final String password, final String firstName, final String lastName, final String email, final boolean active) throws CommonFrameworkException {
+		final UserCreate userCreate = new UserCreate();
+		userCreate.setName(username);
+		userCreate.setPassword(password);
+		userCreate.setEmail(email);
+		userCreate.setFirstName(firstName);
+		userCreate.setLastName(lastName);
+		userCreate.setActive(active);
 
-        UserIdToken userIdToken;
-        try {
-            userIdToken = ccApiWrapper.getUserApi().createUser(userCreate);
-        } catch (SdkFault e) {
-            throw new CommonFrameworkException("Error creating user " + username + ": " + e.getMessage());
-        }
+		UserIdToken userIdToken;
+		try {
+			userIdToken = ccApiWrapper.getUserApi().createUser(userCreate);
+		} catch (final SdkFault e) {
+			throw new CommonFrameworkException("Error creating user " + username + ": " + e.getMessage());
+		}
 
-        return userIdToken.getId();
-    }
+		return userIdToken.getId();
+	}
 
-    @Override
-    public CodeCenterUserPojo getUserByName(String userName) throws CommonFrameworkException {
-        User sdkUser = getSdkUserByNameCached(userName);
-        return toPojo(sdkUser);
-    }
+	@Override
+	public CodeCenterUserPojo getUserByName(final String userName) throws CommonFrameworkException {
+		log.debug("getUserByName(): Username: " + userName);
+		final User sdkUser = getSdkUserByNameCached(userName);
+		return toPojo(sdkUser);
+	}
 
-    @Override
-    public CodeCenterUserPojo getUserById(String userId) throws CommonFrameworkException {
-        User sdkUser = getSdkUserByIdCached(userId);
-        return toPojo(sdkUser);
-    }
+	@Override
+	public CodeCenterUserPojo getUserById(final String userId) throws CommonFrameworkException {
+		final User sdkUser = getSdkUserByIdCached(userId);
+		return toPojo(sdkUser);
+	}
 
-    @Override
-    public void deleteUserById(String userId) throws CommonFrameworkException {
-        removeUserByIdFromCache(userId);
-        UserIdToken userIdToken = new UserIdToken();
-        userIdToken.setId(userId);
-        try {
-            ccApiWrapper.getUserApi().deleteUser(userIdToken);
-        } catch (SdkFault e) {
-            throw new CommonFrameworkException("Error deleting user with ID " + userId + ": " + e.getMessage());
-        }
-    }
+	@Override
+	public void deleteUserById(final String userId) throws CommonFrameworkException {
+		removeUserByIdFromCache(userId);
+		final UserIdToken userIdToken = new UserIdToken();
+		userIdToken.setId(userId);
+		try {
+			ccApiWrapper.getUserApi().deleteUser(userIdToken);
+		} catch (final SdkFault e) {
+			throw new CommonFrameworkException("Error deleting user with ID " + userId + ": " + e.getMessage());
+		}
+	}
 
-    private void removeUserByIdFromCache(String userId) {
-        if (usersByIdCache.containsKey(userId)) {
-            User sdkUser = usersByIdCache.get(userId);
-            usersByIdCache.remove(userId);
-            usersByNameCache.remove(sdkUser.getName().getName());
-        }
-    }
+	private void removeUserByIdFromCache(final String userId) {
+		if (usersByIdCache.containsKey(userId)) {
+			final User sdkUser = usersByIdCache.get(userId);
+			usersByIdCache.remove(userId);
+			usersByNameCache.remove(sdkUser.getName().getName());
+		}
+	}
 
-    @Override
-    public void setUserActiveStatus(String userId, boolean active) throws CommonFrameworkException {
-        User sdkUser = getSdkUserByIdCached(userId);
-        log.info("Setting user " + sdkUser.getId().getId() + " active flag to: " + active);
-        removeUserByIdFromCache(userId);
-        UserUpdate userUpdate = new UserUpdate();
-        userUpdate.setId(sdkUser.getId());
-        userUpdate.setActive(active);
-        try {
-            ccApiWrapper.getUserApi()
-                    .updateUser(userUpdate);
-        } catch (SdkFault e) {
-            throw new CommonFrameworkException("Error setting active status on user ID: " + userId + ": " + e.getMessage());
-        }
+	@Override
+	public void setUserActiveStatus(final String userId, final boolean active) throws CommonFrameworkException {
+		final User sdkUser = getSdkUserByIdCached(userId);
+		log.info("Setting user " + sdkUser.getId().getId() + " active flag to: " + active);
+		removeUserByIdFromCache(userId);
+		final UserUpdate userUpdate = new UserUpdate();
+		userUpdate.setId(sdkUser.getId());
+		userUpdate.setActive(active);
+		try {
+			ccApiWrapper.getUserApi()
+			.updateUser(userUpdate);
+		} catch (final SdkFault e) {
+			throw new CommonFrameworkException("Error setting active status on user ID: " + userId + ": " + e.getMessage());
+		}
 
-    }
+	}
 
-    private CodeCenterUserPojo toPojo(User sdkUser) {
-        return new CodeCenterUserPojo(sdkUser.getId().getId(), sdkUser.getName().getName(), sdkUser.getFirstName(), sdkUser.getLastName(), sdkUser.getEmail(),
-                sdkUser.isActive());
-    }
+	private CodeCenterUserPojo toPojo(final User sdkUser) {
+		return new CodeCenterUserPojo(sdkUser.getId().getId(), sdkUser.getName().getName(), sdkUser.getFirstName(), sdkUser.getLastName(), sdkUser.getEmail(),
+				sdkUser.isActive());
+	}
 
-    private User getSdkUserByNameCached(String userName) throws CommonFrameworkException {
-        if (usersByNameCache.containsKey(userName)) {
-            User sdkUser = usersByNameCache.get(userName);
-            return sdkUser;
-        }
-        UserNameToken userNameToken = new UserNameToken();
-        userNameToken.setName(userName);
-        User sdkUser;
-        try {
-            sdkUser = ccApiWrapper.getUserApi().getUser(userNameToken);
-        } catch (SdkFault e) {
-            throw new CommonFrameworkException("Error getting user with name " + userName + ": " + e.getMessage());
-        }
+	private User getSdkUserByNameCached(final String userName) throws CommonFrameworkException {
+		if (usersByNameCache.containsKey(userName)) {
+			final User sdkUser = usersByNameCache.get(userName);
+			return sdkUser;
+		}
+		final UserNameToken userNameToken = new UserNameToken();
+		userNameToken.setName(userName);
+		User sdkUser;
+		try {
+			log.debug("SDK: Getting user: " + userName);
+			sdkUser = ccApiWrapper.getUserApi().getUser(userNameToken);
+			log.debug("SDK: Done getting user");
+		} catch (final SdkFault e) {
+			log.debug("SDK: Error getting user");
+			throw new CommonFrameworkException("Error getting user with name " + userName + ": " + e.getMessage());
+		}
 
-        usersByIdCache.put(sdkUser.getId().getId(), sdkUser);
-        usersByNameCache.put(sdkUser.getName().getName(), sdkUser);
-        return sdkUser;
-    }
+		usersByIdCache.put(sdkUser.getId().getId(), sdkUser);
+		usersByNameCache.put(sdkUser.getName().getName(), sdkUser);
+		return sdkUser;
+	}
 
-    private User getSdkUserByIdCached(String userId) throws CommonFrameworkException {
-        if (usersByIdCache.containsKey(userId)) {
-            User sdkUser = usersByIdCache.get(userId);
-            return sdkUser;
-        }
-        UserIdToken userIdToken = new UserIdToken();
-        userIdToken.setId(userId);
-        User sdkUser;
-        try {
-            sdkUser = ccApiWrapper.getUserApi().getUser(userIdToken);
-        } catch (SdkFault e) {
-            throw new CommonFrameworkException("Error getting user with ID " + userId + ": " + e.getMessage());
-        }
+	private User getSdkUserByIdCached(final String userId) throws CommonFrameworkException {
+		if (usersByIdCache.containsKey(userId)) {
+			final User sdkUser = usersByIdCache.get(userId);
+			return sdkUser;
+		}
+		final UserIdToken userIdToken = new UserIdToken();
+		userIdToken.setId(userId);
+		User sdkUser;
+		try {
+			sdkUser = ccApiWrapper.getUserApi().getUser(userIdToken);
+		} catch (final SdkFault e) {
+			throw new CommonFrameworkException("Error getting user with ID " + userId + ": " + e.getMessage());
+		}
 
-        usersByIdCache.put(sdkUser.getId().getId(), sdkUser);
-        usersByNameCache.put(sdkUser.getName().getName(), sdkUser);
-        return sdkUser;
-    }
+		usersByIdCache.put(sdkUser.getId().getId(), sdkUser);
+		usersByNameCache.put(sdkUser.getName().getName(), sdkUser);
+		return sdkUser;
+	}
 
-    @Override
-    public List<ApplicationRolePojo> getApplicationRolesByUserName(String userName) throws CommonFrameworkException {
-        List<ApplicationRolePojo> appRolePojos = new ArrayList<ApplicationRolePojo>();
+	@Override
+	public List<ApplicationRolePojo> getApplicationRolesByUserName(final String userName) throws CommonFrameworkException {
+		final List<ApplicationRolePojo> appRolePojos = new ArrayList<ApplicationRolePojo>();
 
-        UserNameToken userToken = new UserNameToken();
-        userToken.setName(userName);
-        List<UserRoleAssignment> roles = null;
+		final UserNameToken userToken = new UserNameToken();
+		userToken.setName(userName);
+		List<UserRoleAssignment> roles = null;
 
-        try {
-            roles = ccApiWrapper.getProxy().getRoleApi().getUserRoles(userToken);
-        } catch (SdkFault e) {
-            throw new CommonFrameworkException("Error getting roles for user: " + userName + ": " + e.getMessage());
-        }
+		try {
+			roles = ccApiWrapper.getProxy().getRoleApi().getUserRoles(userToken);
+		} catch (final SdkFault e) {
+			throw new CommonFrameworkException("Error getting roles for user: " + userName + ": " + e.getMessage());
+		}
 
-        for (RoleAssignment role : roles) {
+		for (final RoleAssignment role : roles) {
 
-            if (role instanceof ApplicationRoleAssignment) {
-                ApplicationRoleAssignment appRole = (ApplicationRoleAssignment) role;
-                String appName = appRole.getApplicationNameVersionToken().getName();
-                String appVersion = appRole.getApplicationNameVersionToken().getVersion();
-                String roleName = appRole.getRoleNameToken().getName();
-                log.debug("User " + userName + " has role " + roleName + " on Application " + appName + " / " + appVersion);
-                ApplicationRolePojo appRolePojo = new ApplicationRolePojo(appRole.getApplicationIdToken().getId(), appName, appVersion,
-                        appRole.getUserIdToken().getId(), appRole.getUserNameToken().getName(), appRole.getRoleIdToken().getId(),
-                        roleName);
-                appRolePojos.add(appRolePojo);
-            }
+			if (role instanceof ApplicationRoleAssignment) {
+				final ApplicationRoleAssignment appRole = (ApplicationRoleAssignment) role;
+				final String appName = appRole.getApplicationNameVersionToken().getName();
+				final String appVersion = appRole.getApplicationNameVersionToken().getVersion();
+				final String roleName = appRole.getRoleNameToken().getName();
+				log.debug("User " + userName + " has role " + roleName + " on Application " + appName + " / " + appVersion);
+				final ApplicationRolePojo appRolePojo = new ApplicationRolePojo(appRole.getApplicationIdToken().getId(), appName, appVersion,
+						appRole.getUserIdToken().getId(), appRole.getUserNameToken().getName(), appRole.getRoleIdToken().getId(),
+						roleName);
+				appRolePojos.add(appRolePojo);
+			}
 
-        }
-        return appRolePojos;
-    }
+		}
+		return appRolePojos;
+	}
 
 }
